cc47c89917a2da92cce16287c9dc52f6300257e1
esky2674 <ester.kylmanen.2674@student.uu.se>
2020-04-09 15:40:31 +0200

    navigation bar

--- adminInterface/adminInterface/templatetags/block_tags.py
+++ adminInterface/adminInterface/templatetags/block_tags.py
@@ -2,7 +2,9 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('navigation_bar.html')
-def navigation_bar():
+@register.inclusion_tag('navigation.html')
+def navigation():
     data = ['start','page2']
     return {'links':data}
+
+# funkar ej, raderar om jag inte får det att funka 
