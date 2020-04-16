51d5641acc690c42780ca6afbe63662e5ff37567
Daniel Fehrm <daniel@fehrm.se>
2020-04-16 20:22:30 +0200

    Properly include navigation bar

--- adminInterface/adminInterface/templatetags/block_tags.py
+++ adminInterface/adminInterface/templatetags/block_tags.py
@@ -2,9 +2,15 @@ from django import template
 
 register = template.Library()
 
-@register.inclusion_tag('navigation.html')
-def navigation():
-    data = ['start','page2']
-    return {'links':data}
+@register.inclusion_tag("navigation_bar.html", takes_context=True)
+def navigation_bar(context):
+    links = {
+        'Start': "/start",
+        'Nåt annat': "/page2"
+    }
+    return {
+        'links': links,
+        'request': context['request']
+    }
 
 # funkar ej, raderar om jag inte får det att funka 
