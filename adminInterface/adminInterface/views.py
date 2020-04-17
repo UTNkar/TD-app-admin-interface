daeb0a007e0083e3c44b00ffc793cfbbd5608155
Daniel Fehrm <daniel@fehrm.se>
2020-04-17 16:06:58 +0200

    Fix linting

--- adminInterface/adminInterface/views.py
+++ adminInterface/adminInterface/views.py
@@ -1,32 +1,35 @@
 import pyrebase
 import os
-from django.shortcuts import render, redirect 
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 
 
 config = {
-'apiKey': os.environ.get("FIREBASE_APIKEY"),
-'authDomain': os.environ.get("FIREBASE_AUTHDOMAIN"),
-'databaseURL': os.environ.get("FIREBASE_DATABASEURL"),
-'projectId': os.environ.get("FIREBASE_PROJECTID"),
-'storageBucket': os.environ.get("FIREBASE_STORGAEBUCKET")
+    'apiKey': os.environ.get("FIREBASE_APIKEY"),
+    'authDomain': os.environ.get("FIREBASE_AUTHDOMAIN"),
+    'databaseURL': os.environ.get("FIREBASE_DATABASEURL"),
+    'projectId': os.environ.get("FIREBASE_PROJECTID"),
+    'storageBucket': os.environ.get("FIREBASE_STORGAEBUCKET")
 }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
+
+
 def singIn(request):
     return render(request, "signIn.html")
+
 
 @login_required
 def page2(request):
     return render(request, "page2.html")
 
+
 @login_required
 def start(request):
     return render(request, "welcome.html")
+
 
 def login_user(request):
     uname = request.POST.get('username')
@@ -37,9 +40,4 @@ def login_user(request):
         return redirect('/start')
     else:
         message = "invalid cerediantials"
-        return redirect('/')
-        
-    
-    
-        
-
+        return redirect('/', {'msg': message})
