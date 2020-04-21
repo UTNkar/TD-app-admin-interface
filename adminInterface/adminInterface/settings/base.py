11af55c49e043bf1237e0f774aff37aa4922e3a9
Daniel Fehrm <daniel@fehrm.se>
2020-04-21 16:50:10 +0200

    Split up settings for production

--- adminInterface/adminInterface/settings/base.py
+++ adminInterface/adminInterface/settings/base.py
@@ -15,19 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'deg=*gpfq%ymtn-!ws#_g^-4r94cn=3n^=w!yig+6u%=hz#5b0'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -72,16 +59,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'adminInterface.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
