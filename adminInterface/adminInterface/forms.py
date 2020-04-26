472cec3f4b994d79f8ada9a91a1e0b706cd9a09e
esky2674 <ester.kylmanen.2674@student.uu.se>
2020-04-26 15:24:04 +0200

    deleted name in form

--- adminInterface/adminInterface/forms.py
+++ adminInterface/adminInterface/forms.py
@@ -27,7 +27,8 @@ class EventForm(ModelForm):
 
     class Meta:
         model = Event
-        fields = ['name',
+        fields = ['id',
+                  'name',
                   'disappear',
                   'form',
                   'release',
@@ -35,14 +36,13 @@ class EventForm(ModelForm):
 
     def save(self):
         data = self.cleaned_data
-        print(data.get('name'))
         db = Firestore.get_instance()
         event_name = data.get('name')
         doc_ref = db.collection(u'event').document(event_name)
         doc_ref.set({
-            u'name': data.get('name'),
             u'disappear': data.get('disappear'),
             u'form': data.get('form'),
             u'release': data.get('release'),
             u'who': data.get('who')
         })
+        return data
