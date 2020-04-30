1285b0037700d4905c48371a5ef293576f2c5e55
Simon Jaklovsky <simon.jaklovsky@gmail.com>
2020-04-30 15:34:45 +0200

    Added firebase ID to Section model

--- adminInterface/adminInterface/models.py
+++ adminInterface/adminInterface/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Section(models.Model):
+    firebase_id = models.CharField(max_length=60)
     sectionName = models.CharField(max_length=5)
     sectionFullName = models.CharField(max_length=60)
     classes = []
