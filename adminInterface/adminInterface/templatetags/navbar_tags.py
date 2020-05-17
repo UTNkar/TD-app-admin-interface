be2079a50910057fb0affe392f3aa1684b51be99
SimonJaklovsky <61022788+SimonJaklovsky@users.noreply.github.com>
2020-05-17 14:56:30 +0200

    Update adminInterface/adminInterface/templatetags/navbar_tags.py
    Co-authored-by: Daniel Fehrm <daniel@fehrm.se>

--- adminInterface/adminInterface/templatetags/navbar_tags.py
+++ adminInterface/adminInterface/templatetags/navbar_tags.py
@@ -8,7 +8,7 @@ def navigation_bar(context):
     # The links must have the title as the key and the link as the value
     links = {
         'Start': "/start",
-        'Sections': "/sections",
+        'Sektioner': "/sections",
         'Biljettsläpp': "/ticket-system"
     }
     return {
