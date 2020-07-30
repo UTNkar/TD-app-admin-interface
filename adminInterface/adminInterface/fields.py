5d8969499f631820e30db3913487086ba4f13ad8
Daniel Fehrm <daniel@fehrm.se>
2020-07-30 13:21:40 +0200

    fix linting issues

--- adminInterface/adminInterface/fields.py
+++ adminInterface/adminInterface/fields.py
@@ -6,7 +6,7 @@ class DataListWidget(forms.TextInput):
         super(DataListWidget, self).__init__(*args, **kwargs)
         self._name = name
         self._list = data_list
-        self.attrs.update({'list':'list__%s' % self._name})
+        self.attrs.update({'list': 'list__%s' % self._name})
 
     def render(self, name, value, attrs=None, renderer=None):
         text_html = super(DataListWidget, self).render(
