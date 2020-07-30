08019c1ac8f0117839720d82c10ba439cff2e6a6
Daniel Fehrm <daniel@fehrm.se>
2020-07-30 13:32:33 +0200

    added django cron

--- adminInterface/adminInterface/settings/base.py
+++ adminInterface/adminInterface/settings/base.py
@@ -25,7 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'multiselectfield'
+    'multiselectfield',
+    'django_cron',
 ]
 
 MIDDLEWARE = [
@@ -36,7 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
 ]
 
 ROOT_URLCONF = 'adminInterface.urls'
