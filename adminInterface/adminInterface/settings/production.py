bcc2c436fa42faa247a9b89e7ef0adfcc9e6bb5b
Daniel Fehrm <daniel@fehrm.se>
2020-08-03 17:35:29 +0200

    Forgot important && after kronos prefix

--- adminInterface/adminInterface/settings/production.py
+++ adminInterface/adminInterface/settings/production.py
@@ -36,4 +36,4 @@ sentry_sdk.init(
     send_default_pii=True
 )
 
-KRONOS_PREFIX = 'export GOOGLE_APPLICATION_CREDENTIALS="admininterface-privatekey.json"'
+KRONOS_PREFIX = 'export GOOGLE_APPLICATION_CREDENTIALS="admininterface-privatekey.json" &&'
